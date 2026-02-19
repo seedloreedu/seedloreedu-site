38299d30380862e06710c2ce03880019d13ccc9e
seedloreedu <seedloreeducare@gmail.com>
2026-02-19 17:29:53 +0530

    whats app feature

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,20 @@ export default function MyApp({ Component, pageProps }) {
   return (
     <GlobalProvider>
       <Component {...pageProps} />
+
+      {/* WhatsApp Floating Button */}
+      <a
+        href="https://wa.me/916290465739?text=Hi%20Seedlore%20Educare,%20I%20am%20interested%20in%20Robotics%20classes."
+        target="_blank"
+        rel="noopener noreferrer"
+        className="whatsapp-float"
+      >
+        <img
+          src="/images/whatsapp.png"
+          alt="Chat on WhatsApp"
+        />
+      </a>
+
     </GlobalProvider>
   )
 }
